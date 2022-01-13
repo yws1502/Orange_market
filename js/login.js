c778f19d4de8249c8070df83f9b392e80d585ece
Woosang <yws1502@naver.com>
2022-01-13 17:11:05 +0900

    ✨ 로그인 API 연동

--- js/login.js
+++ js/login.js
@@ -1,51 +1,90 @@
-// join page path
-const joinPath = "/pages/join.html";
+const ENDPOINT = "http://146.56.183.55:5050";
+const HEADERS = {
+  "Content-type" : "application/json"
+};
 
-// 초기 화면 3초 동안 보여주기
-const splash = document.querySelector(".splash");
 const selectLogin = document.querySelector(".select-login");
 const loginSection = document.querySelector(".login");
+const selectLoginBtnWrap = selectLogin.querySelector(".btn-wrap")
+const loginForm = loginSection.querySelector("form");
+const loginInputs = loginSection.querySelectorAll("input");
+const loginBtn = loginSection.querySelector("form button");
+
 
 // hello orange market
 setTimeout(() => {
-  splash.classList.add("hidden");
-  selectLogin.classList.remove("hidden");
-}, 700);
+  selectLogin.classList.remove("show-effect");
+}, 2100);
 
-// 이메일 로그인, 회원가입으로 이동하는 버튼
-const selectBtnWrap = selectLogin.querySelector(".btn-wrap")
-
-selectBtnWrap.addEventListener("click", (event) => {
+// 화면 이동 함수 (이메일 로그인, 회원가입)
+function changeLoginWindow(event) {
   const className = event.target.className;
+  
   if (className === "login-btn") {
     selectLogin.classList.add("hidden");
     loginSection.classList.remove("hidden");
   } else if (className === "join-btn") {
-    location.href = joinPath;
-  }
-});
-
-// 로그인 화면에서 회원가입 화면으로 이동
-const joinLinkBtn = loginSection.querySelector(".link-btn-join");
-
-joinLinkBtn.addEventListener("click", () => {
-  location.href = joinPath;
-});
-
-// 로그인 input에 값 입력하면 btn 활성화
-const loginForm = loginSection.querySelector("form");
-const emailInput = loginSection.querySelector("#Email");
-const pwdInput = loginSection.querySelector("#Pwd")
-const loginBtn = loginSection.querySelector("form button");
-
-function stateHandle() {
-  if (emailInput.value === "" || pwdInput.value === "") {
-    loginBtn.disabled = true;
-    loginBtn.classList = "l-disabled-button";
-  } else {
-    loginBtn.disabled = false;
-    loginBtn.classList = "l-button";
+    location.href = "/pages/join.html";
   }
 }
 
-loginForm.addEventListener("change", stateHandle);
+// 화면 이동 함수 (이메일 로그인, 회원가입)
+selectLoginBtnWrap.addEventListener("click", changeLoginWindow);
+
+
+// 로그인 화면에서 회원가입 화면으로 이동
+document.querySelector(".login .link-btn-join").addEventListener("click", () => {
+  location.href = "/pages/join.html";
+});
+
+// 로그인 input에 값 입력하면 btn 활성화
+function stateHandle() {
+  let check = 0;
+  loginInputs.forEach((el) => {if (el.value !== "") check++})
+
+  if (check === loginInputs.length) {
+    loginBtn.disabled = false;
+    loginBtn.classList = "l-button";
+  } else {
+    loginBtn.disabled = true;
+    loginBtn.classList = "l-disabled-button";
+  }
+}
+
+// 입력값들이 채워지면 버튼 활성화
+loginForm.addEventListener("keyup", stateHandle)
+
+
+// 로그인 통신
+async function loginApi(e) {
+  e.preventDefault();
+  const user = {};
+  loginInputs.forEach((el) => {user[el.name] = el.value});
+  
+  const reqOption = {
+    method: "POST",
+    headers: HEADERS,
+    body: JSON.stringify({user})
+  };
+
+  const res = await fetch(`${ENDPOINT}/user/login`, reqOption);
+  const json = await res.json();
+
+  if (json.status === 422) {
+    // 화면에 에러 메세지 동적 생성
+    const pwdWrap = document.querySelector(".pwd-wrap");
+    pwdWrap.classList.add("err");
+    const errorMsg = document.createElement("strong");
+    errorMsg.className = "err-msg";
+    errorMsg.textContent = `*${json.message}`;
+    pwdWrap.appendChild(errorMsg);
+    alert(json.message);
+  } else {
+    // 토큰 저장 후 메인 피드 화면으로 이동
+    localStorage.setItem("TOKEN", json.user.token);
+    location.href = "/index.html";
+  }
+};
+
+// 로그인 기능 구현
+loginBtn.addEventListener("click", loginApi);
