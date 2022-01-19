c0e772d69a6c0de271c293fc4908fd14e759814b
Woosang <yws1502@naver.com>
2022-01-19 17:53:03 +0900

    ✨ 유저 검색 기능 추가

---
+++ js/search.js
@@ -0,0 +1,71 @@
+const TOKEN = localStorage.getItem("TOKEN");
+const ENDPOINT = "http://146.56.183.55:5050";
+const SEARCH_API = ENDPOINT + "/user/searchuser/?keyword=";
+const defaultImage = "http://146.56.183.55:5050/Ellipse.png";
+
+const HEADERS = {
+  "Authorization" : `Bearer ${TOKEN}`,
+  "Content-type" : "application/json"
+};
+
+// 기존에 있던 목록 지우기
+function removeAllChilden(parentNode) {
+  while (parentNode.hasChildNodes()) {
+    parentNode.removeChild(parentNode.firstChild);
+  } 
+}
+
+// 검색 결과 유저 정보 화면에 보여주기
+async function paintUserList(event) {
+  const userList = document.querySelector(".search-user-list");
+  removeAllChilden(userList);
+  const reqOption = {
+    method: "GET",
+    headers: HEADERS
+  };
+  const searchValue = event.target.value;
+  const res = await fetch(`${SEARCH_API}${searchValue}`, reqOption);
+  const json = await res.json();
+
+  if (json[0]) {
+    const frag = document.createDocumentFragment("ul");
+      json.forEach((user) => {
+        const { accountname, image, username, _id } = user;
+
+        // 검색 키워드 하이라이트
+        const userName = username.replace(
+          searchValue,
+          `<span class="highlight">${searchValue}</span>`
+        );
+
+        // 잘못된 이미지 경로 예외 처리
+        const userImageUrl = 
+        (image.match(/http:\/\/[0-9].*:5050\//) && !image.match(/undefined/))
+        ? image
+        : defaultImage;
+  
+        const li = document.createElement("li");
+        li.className = "user-search";
+        li.innerHTML = `
+          <a href="/views/your_profile.html?id=${_id}">
+            <img src=${userImageUrl} alt="프로필 사진" class="avatar-img">
+            <p class="user-info">
+              <strong>${userName}</strong>
+              <span>@ ${accountname}</span>
+            </p>
+          </a>
+        `;
+        frag.appendChild(li);
+      });
+    userList.appendChild(frag);
+  }
+};
+
+document.querySelector("form").addEventListener("keyup", paintUserList);
+
+// 뒤로 가기 버튼
+const prevBtn = document.querySelector(".prev-btn");
+
+prevBtn.addEventListener("click", () => {
+  history.back();
+});
