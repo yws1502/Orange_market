73044e91d72bb0d4dedf6556c260929caaa819dc
Woosang <yws1502@naver.com>
2022-01-20 11:01:02 +0900

    ✨ 메인 피드 좋아요 기능 구현

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ window.onload = async () => {
     const feedList = document.querySelector(".feed-list");
 
     json.posts.forEach((post) => {
-      const { author, comments, content, createdAt, heartCount, hearted } = post;
+      const { author, commentCount, content, createdAt, heartCount, hearted } = post;
       const postId = post.id;
       const postImage = post.image;
       const { accountname, username } = author;
@@ -45,13 +45,13 @@ window.onload = async () => {
         : "heart-btn";
 
       // 잘못된 이미지 경로 기본 이미지로 바꿔주기
-      const profileImageUrl = (userImage.match(/https*:\/\/[0-9.]*:5050/) !== null)
+      const userImageUrl = (userImage.match(/https*:\/\/[0-9.]*:5050/) !== null)
         ? userImage
         : "http://146.56.183.55:5050/Ellipse.png";
 
       feedList.innerHTML += `
-        <li id=post${postId} class="home-post">
-          <img src=${profileImageUrl} alt="프로필 사진" class="avatar-img">
+        <li id=post-${post.id} class="home-post">
+          <img src=${userImageUrl} alt="프로필 사진" class="avatar-img">
           <div class="content-wrap">
             <p class="text-wrap">
               <a href="/pages/profile_detail.html?id=${accountname}">
@@ -86,7 +86,7 @@ window.onload = async () => {
         paintPostImage(postImageList, postId);
 
         // 각 포스트의 이미지 슬라이드 wrapper 총 길이 계산
-        const slideList = document.querySelector(`#post${postId} .slide-list`);
+        const slideList = document.querySelector(`#post-${postId} .slide-list`);
         const slideAll = slideList.querySelectorAll(".slide");
 
         let slideCount = slideAll.length;
@@ -98,8 +98,7 @@ window.onload = async () => {
 
 // 포스트 이미지 생성 함수
 function paintPostImage(postImageList, postId) {
-  const slideWrapper = document.querySelector(`#post${postId} .slide-wrapper`);
-
+  const slideWrapper = document.querySelector(`#post-${postId} .slide-wrapper`);
   const slideList = document.createElement("ul");
   const controlBtns = document.createElement("div");
   slideList.className = "slide-list";
@@ -140,36 +139,81 @@ function moveSlide(num, slideList) {
   slideList.style.left = -num * (slideWidth + slideMargin) + "px";
 }
 
-// 이미지 슬라이드 이벤트
+function slideAnimation(Node) {
+  const ControlList = Node.parentElement; // .control-btns div
+  const ControlBtnAll = ControlList.querySelectorAll("button"); // button list
+  const slideList = ControlList.previousElementSibling; // .slide-list ul
+
+  // 이전에 선택되어 있던 위치 찾기
+  let prevIdx;
+  ControlBtnAll.forEach((el, idx) => {
+    if (el.className === "on") prevIdx = idx;
+  });
+  ControlBtnAll[prevIdx].classList.remove("on");
+  Node.classList.add("on");
+
+  switch (Node.name) {
+    case "one":
+      moveSlide(0, slideList);
+      break;
+    case "two":
+      moveSlide(1, slideList);
+      break;
+    case "three":
+      moveSlide(2, slideList);
+      break;
+  }
+}
+
+// 좋아요 기능 통신
+async function heartAPI(route, method, postId, count) {
+  const reqOption = {
+    method: method,
+    headers: HEADERS
+  }
+  const res = await fetch(`${ENDPOINT}/post/${postId}/${route}`, reqOption);
+  const json = await res.json();
+  if (json.status === 404) {
+    alert("존재하지 않는 게시글입니다.");
+    location.href = "/pages/404.html";
+  }
+  const resultCount = (method === "POST") ? +count + 1 : +count - 1
+  return resultCount
+}
+
+
+async function paintHeart(Node, postId) {
+  let heartCount;
+  if (Node.className.includes("on")) {
+    // 좋아요 취소
+    heartCount = await heartAPI("unheart", "DELETE", postId, Node.textContent)
+  } else {
+    // 좋아요
+    heartCount = await heartAPI("heart", "POST", postId, Node.textContent)
+  }
+  Node.classList.toggle("on");
+  Node.textContent = heartCount;
+}
+
+// 각 포스트 이벤트
 const feedList = document.querySelector(".feed-list");
 feedList.addEventListener("click", (event) => {
   const currentNode = event.target;
-  // 이미지 슬라이드 버튼 이벤트 찾기
+  const currentPost = event.path.find((node) => {
+    return node.id.split("-")[0] === "post"
+  });
+  const postId = currentPost.id.split("-")[1];
+
   if (currentNode.parentElement.className === "control-btns") {
-    const ControlList = currentNode.parentElement; // .control-btns div
-    const ControlBtnAll = ControlList.querySelectorAll("button"); // button list
-    const slideList = ControlList.previousElementSibling; // .slide-list ul
+    slideAnimation(currentNode);
 
-    // 이전에 선택되어 있던 위치 찾기
-    let prevIdx;
-    ControlBtnAll.forEach((el, idx) => {
-      if (el.className === "on") prevIdx = idx;
-    });
-    ControlBtnAll[prevIdx].classList.remove("on");
-    currentNode.classList.add("on");
+  } else if (currentNode.className.includes("heart-btn")) {
+    paintHeart(currentNode, postId);
 
-    switch (currentNode.name) {
-      case "one":
-        moveSlide(0, slideList);
-        break;
-      case "two":
-        moveSlide(1, slideList);
-        break;
-      case "three":
-        moveSlide(2, slideList);
-        break;
+  } else if (currentNode.className === "comment-btn"
+    || currentNode.tagName === "IMG") {
+      // location.href = `/pages/post_detail.html/?id=${test}`
     }
-  }
 });
 
 // 검색화면으로 이동 버튼
