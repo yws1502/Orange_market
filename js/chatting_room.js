d3a0f284ce7e6b0215eb6c9c93ffc0687c4254f5
Woosang <yws1502@naver.com>
2022-02-04 11:36:52 +0900

    ✨ 채팅룸 스크롤 하단 고정 기능 추가

---
+++ js/chatting_room.js
@@ -0,0 +1,54 @@
+const $settingMoreBtn = document.querySelector(".more-btn");
+const $modal = document.querySelector(".modal");
+const $chatroom = document.querySelector(".chatting-room");
+const $chatContainer = $chatroom.querySelector(".chatting-room .chatting-cont");
+const $chatTextInput = document.querySelector("#chatTextInput");
+const $chatSubmitBtn = document.querySelector(".chat-submit-btn");
+const $scroll = document.querySelector("html");
+
+$scroll.scrollTop = $scroll.scrollHeight;
+
+$settingMoreBtn.addEventListener("click", () => {
+  $modal.classList.remove("hidden");
+  $modal.addEventListener("click", (event) => {
+    const currentNode = event.target;
+    if (currentNode.className === "modal") {
+      $modal.classList.add("hidden");
+    } else if (currentNode.tagName === "BUTTON") {
+      location.href = "/pages/chatting_list.html";
+    }
+  });
+});
+
+$chatTextInput.addEventListener("keyup", () => {
+  if ($chatTextInput.value) {
+    $chatSubmitBtn.classList.add("on");
+    $chatSubmitBtn.disabled = false;
+  }
+});
+
+$chatSubmitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const li = document.createElement("li");
+  const date = new Date();
+  const hour = date.getHours();
+  const min =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  const currentTime = `${hour}:${min}`;
+
+  li.className = "my-user";
+  li.innerHTML = `
+        <p class="sended-text">${$chatTextInput.value}</p>
+        <em class="sended-time">${currentTime}</em>
+      `;
+  $chatContainer.appendChild(li);
+  $scroll.scrollTop = $scroll.scrollHeight;
+  $chatTextInput.value = "";
+});
+
+// 뒤로 가기 버튼
+const prevBtn = document.querySelector(".prev-btn");
+
+prevBtn.addEventListener("click", () => {
+  history.back();
+});
